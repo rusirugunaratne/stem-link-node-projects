9a5eb4839a7756a2a4baa7e08cce40afb38ab0fd
Rusiru Gunaratne <info.rusiru@gmail.com>
2026-05-24 10:16:13 +0530

    Added route params

--- sample-projects/node-d01-example/src/server.ts
+++ sample-projects/node-d01-example/src/server.ts
@@ -8,6 +8,16 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello world from our express app");
 });
 
+app.get("/students", (req: Request, res: Response) => {
+  res.send("Some students data");
+});
+
+// /students/12
+app.get("/students/:id", (req: Request, res: Response) => {
+  const studentId = req.params.id;
+  res.send(`Student data for student with id ${studentId}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
