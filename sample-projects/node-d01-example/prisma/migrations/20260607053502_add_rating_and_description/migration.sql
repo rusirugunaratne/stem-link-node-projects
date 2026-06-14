ba7f95fe260fdfd6c0066db1a764c09336a15a00
Rusiru Gunaratne <info.rusiru@gmail.com>
2026-06-14 08:50:38 +0530

    Files cleaned

--- sample-projects/node-d01-example/prisma/migrations/20260607053502_add_rating_and_description/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Movie" ADD COLUMN     "description" TEXT NOT NULL DEFAULT 'Description not available',
-ADD COLUMN     "rating" DOUBLE PRECISION NOT NULL DEFAULT 0.0;
--- sample-projects/node-d01-example/src/routes/index.ts
+++ sample-projects/node-d01-example/src/routes/index.ts
@@ -2,8 +2,4 @@ import { Router } from "express";
 
 const globalRouter = Router();
 
-// Concept for Students: Central hub for all feature sub-routes
-// globalRouter.use("/users", userRouter);
-// globalRouter.use("/submissions", submissionRouter);
-
 export default globalRouter;
